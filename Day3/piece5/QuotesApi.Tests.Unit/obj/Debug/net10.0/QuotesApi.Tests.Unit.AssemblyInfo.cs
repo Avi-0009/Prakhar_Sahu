6db384c05fe6f0ff3975d150ff004ffc45db9dee
Avi-0009 <prakharsahu1229@gmail.com>
2026-08-14 15:20:46 +0530

    feat: configure azd for azure container apps deployment

--- Day3/piece5/QuotesApi.Tests.Unit/obj/Debug/net10.0/QuotesApi.Tests.Unit.AssemblyInfo.cs
+++ Day3/piece5/QuotesApi.Tests.Unit/obj/Debug/net10.0/QuotesApi.Tests.Unit.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuotesApi.Tests.Unit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8e10225a7bb5341e6b211dd4e03d82f1aff177c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8720376b143a30728ef986e65b9cb591b4671ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuotesApi.Tests.Unit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuotesApi.Tests.Unit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
